586db6603e8765ff77c38339c5492100db7c3086
saad <saadneosoft@gmail.com>
2023-11-20 15:42:02 +0530

    Initial Commit

---
+++ MapKitInSwiftUi/MapKitInSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitInSwiftUiApp.swift
+//  MapKitInSwiftUi
+//
+//  Created by Neosoft on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MapKitInSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
