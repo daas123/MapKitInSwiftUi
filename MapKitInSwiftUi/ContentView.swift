898d5bf8855b0c878bd149b0a489f5bd885008f7
saad <saadneosoft@gmail.com>
2023-11-21 11:59:13 +0530

    basic userloction

--- MapKitInSwiftUi/ContentView.swift
+++ MapKitInSwiftUi/ContentView.swift
@@ -1,21 +1,51 @@
-//
-//  ContentView.swift
-//  MapKitInSwiftUi
-//
-//  Created by Neosoft on 20/11/23.
-//
-
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @StateObject private var locationManager = LocationManager()
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),
+        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    )
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: $userTrackingMode)
+            .onAppear {
+                // If you need initial location update
+                if let userLocation = locationManager.userLocation {
+                    region.center = userLocation
+                }
+            }
+            .onReceive(locationManager.$userLocation) { newLocation in
+                if let location = newLocation {
+                    region.center = location
+                }
+            }
+    }
+}
+
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private var locationManager = CLLocationManager()
+    
+    @Published var userLocation: CLLocationCoordinate2D?
+    
+    override init() {
+        super.init()
+        setupLocationManager()
+    }
+    
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first?.coordinate {
+            userLocation = location
         }
-        .padding()
     }
 }
 
