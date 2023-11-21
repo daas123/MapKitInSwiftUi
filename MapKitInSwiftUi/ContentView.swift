772daf767e2c9a548c54417433239f3544fc51c6
saad <saadneosoft@gmail.com>
2023-11-21 12:44:54 +0530

    finding nearby returent

--- MapKitInSwiftUi/ContentView.swift
+++ MapKitInSwiftUi/ContentView.swift
@@ -1,23 +1,99 @@
-//
-//  ContentView.swift
-//  MapKitInSwiftUi
-//
-//  Created by Neosoft on 20/11/23.
-//
-
 import SwiftUI
+import MapKit
+import CoreLocation
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private var locationManager = CLLocationManager()
+    @Published var mapItems: [MKMapItem] = []
+
+    override init() {
+        super.init()
+        self.locationManager.delegate = self
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        self.locationManager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        searchForRestaurants(at: location.coordinate)
+    }
+
+    func searchForRestaurants(at coordinate: CLLocationCoordinate2D) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "restaurant"
+        request.region = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            guard let response = response, error == nil else {
+                // Handle error
+                return
+            }
+
+            self.mapItems = response.mapItems
         }
-        .padding()
     }
 }
+
+struct ContentView: View {
+    @StateObject private var locationManager = LocationManager()
+
+    var body: some View {
+        VStack {
+            MapView(mapItems: $locationManager.mapItems)
+                .edgesIgnoringSafeArea(.all)
+        }
+    }
+}
+
+struct MapView: UIViewRepresentable {
+    @Binding var mapItems: [MKMapItem]
+
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.showsUserLocation = true
+        mapView.delegate = context.coordinator
+        return mapView
+    }
+
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        uiView.removeAnnotations(uiView.annotations)
+
+        for item in mapItems {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = item.placemark.coordinate
+            annotation.title = item.name
+            uiView.addAnnotation(annotation)
+        }
+
+        if let firstItem = mapItems.first {
+            let region = MKCoordinateRegion(
+                center: firstItem.placemark.coordinate,
+                span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            )
+            uiView.setRegion(region, animated: true)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            // Customize the annotation view if needed
+            return nil
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
